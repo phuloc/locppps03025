9f91d5fd0a251d7d2528026eb03a6c293d5bcc99
phuloc <locpps@gmail.com>
2016-03-04 08:27:09 -0800

    đầy lên

---
+++ assignment/frmMain.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.KhácHàngToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChỉnhSữaKHToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.XemKhácHàngToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SảnPhẩmToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ThêmSảnPhẩmToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.XemSảnPhẩmToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LiênHệToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GiớiThiệuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'KhácHàngToolStripMenuItem
+        '
+        Me.KhácHàngToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ChỉnhSữaKHToolStripMenuItem, Me.XemKhácHàngToolStripMenuItem})
+        Me.KhácHàngToolStripMenuItem.Name = "KhácHàngToolStripMenuItem"
+        Me.KhácHàngToolStripMenuItem.Size = New System.Drawing.Size(75, 20)
+        Me.KhácHàngToolStripMenuItem.Text = "Khác hàng"
+        '
+        'ChỉnhSữaKHToolStripMenuItem
+        '
+        Me.ChỉnhSữaKHToolStripMenuItem.Name = "ChỉnhSữaKHToolStripMenuItem"
+        Me.ChỉnhSữaKHToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.ChỉnhSữaKHToolStripMenuItem.Text = "Cập nhật KH"
+        '
+        'XemKhácHàngToolStripMenuItem
+        '
+        Me.XemKhácHàngToolStripMenuItem.Name = "XemKhácHàngToolStripMenuItem"
+        Me.XemKhácHàngToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.XemKhácHàngToolStripMenuItem.Text = "Xem khác hàng"
+        '
+        'SảnPhẩmToolStripMenuItem
+        '
+        Me.SảnPhẩmToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ThêmSảnPhẩmToolStripMenuItem, Me.XemSảnPhẩmToolStripMenuItem})
+        Me.SảnPhẩmToolStripMenuItem.Name = "SảnPhẩmToolStripMenuItem"
+        Me.SảnPhẩmToolStripMenuItem.Size = New System.Drawing.Size(72, 20)
+        Me.SảnPhẩmToolStripMenuItem.Text = "Sản phẩm"
+        '
+        'ThêmSảnPhẩmToolStripMenuItem
+        '
+        Me.ThêmSảnPhẩmToolStripMenuItem.Name = "ThêmSảnPhẩmToolStripMenuItem"
+        Me.ThêmSảnPhẩmToolStripMenuItem.Size = New System.Drawing.Size(153, 22)
+        Me.ThêmSảnPhẩmToolStripMenuItem.Text = "Cập nhật SP"
+        '
+        'XemSảnPhẩmToolStripMenuItem
+        '
+        Me.XemSảnPhẩmToolStripMenuItem.Name = "XemSảnPhẩmToolStripMenuItem"
+        Me.XemSảnPhẩmToolStripMenuItem.Size = New System.Drawing.Size(153, 22)
+        Me.XemSảnPhẩmToolStripMenuItem.Text = "Xem sản phẩm"
+        '
+        'LiênHệToolStripMenuItem
+        '
+        Me.LiênHệToolStripMenuItem.Name = "LiênHệToolStripMenuItem"
+        Me.LiênHệToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.LiênHệToolStripMenuItem.Text = "Liên hệ"
+        '
+        'GiớiThiệuToolStripMenuItem
+        '
+        Me.GiớiThiệuToolStripMenuItem.Name = "GiớiThiệuToolStripMenuItem"
+        Me.GiớiThiệuToolStripMenuItem.Size = New System.Drawing.Size(70, 20)
+        Me.GiớiThiệuToolStripMenuItem.Text = "Giới thiệu"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KhácHàngToolStripMenuItem, Me.SảnPhẩmToolStripMenuItem, Me.LiênHệToolStripMenuItem, Me.GiớiThiệuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(570, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(490, 261)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(80, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "LOCPS03025"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.assignment.My.Resources.Resources.Chrysanthemum
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(570, 283)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMain"
+        Me.Text = "Điện toán đám mây"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents KhácHàngToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ChỉnhSữaKHToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents XemKhácHàngToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SảnPhẩmToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ThêmSảnPhẩmToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents XemSảnPhẩmToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LiênHệToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GiớiThiệuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
